26c88683eb8b6ff13d969fcd31ea1300b558a8b9
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2021-11-11 19:24:09 +0100

    Install stitches.js

---
+++ stitches.config.ts
@@ -0,0 +1,28 @@
+import { createStitches } from '@stitches/react';
+
+const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    colors: {},
+  },
+  media: {},
+});
+
+export {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+};
