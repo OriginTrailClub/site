b1a0da85a9eeb798f4e8c5a5027e6a02492682f7
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2021-11-11 19:37:41 +0100

    Load necessary fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,30 @@
 import type { AppProps } from 'next/app';
 
-import { globalCss } from "stitches.config";
+import { globalCss } from 'stitches.config';
 
 const globalStyles = globalCss({
   html: {
-    height: "100%",
-    width: "100%",
+    height: '100%',
+    width: '100%',
   },
   body: {
     margin: 0,
-    minHeight: "100%",
-    width: "100%",
-    fontFamily: "$default",
-    color: "$text-body",
-    "-webkit-font-smoothing": "antialiased",
-    "-moz-osx-font-smoothing": "grayscale",
+    minHeight: '100%',
+    width: '100%',
+    fontFamily: '$default',
+    color: '$text-body',
+    '-webkit-font-smoothing': 'antialiased',
+    '-moz-osx-font-smoothing': 'grayscale',
   },
-  "#__next": {
-    minHeight: "100%",
-    width: "100%",
+  '#__next': {
+    minHeight: '100%',
+    width: '100%',
   },
-  "*": {
-    boxSizing: "border-box",
-    position: "relative",
+  '*': {
+    boxSizing: 'border-box',
+    position: 'relative',
   },
 });
-
 
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { getCssText } from 'stitches.config';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;700&family=Lato:ital@0;1&family=Poppins:wght@500;600;700;800&display=swap"
+            rel="stylesheet"
+          />
+          <style
+            id="stitches"
+            dangerouslySetInnerHTML={{ __html: getCssText() }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
