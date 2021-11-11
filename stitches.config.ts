b92f5f786ff55c6ba7c24894b3b46f3bcc83224f
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2021-11-11 19:28:28 +0100

    Determine breakpoints

--- stitches.config.ts
+++ stitches.config.ts
@@ -13,7 +13,12 @@ const {
   theme: {
     colors: {},
   },
-  media: {},
+  media: {
+    bp1: '(min-width: 480px)',
+    bp2: '(min-width: 640px)',
+    bp3: '(min-width: 768px)',
+    bp4: '(min-width: 1024px)',
+  },
 });
 
 export {
