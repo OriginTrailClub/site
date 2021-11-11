970bdae3bcbe95e59af8b0e510c0aa520f723a8a
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2021-11-11 19:33:25 +0100

    Add global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import { globalCss } from "stitches.config";
+
+const globalStyles = globalCss({
+  html: {
+    height: "100%",
+    width: "100%",
+  },
+  body: {
+    margin: 0,
+    minHeight: "100%",
+    width: "100%",
+    fontFamily: "$default",
+    color: "$text-body",
+    "-webkit-font-smoothing": "antialiased",
+    "-moz-osx-font-smoothing": "grayscale",
+  },
+  "#__next": {
+    minHeight: "100%",
+    width: "100%",
+  },
+  "*": {
+    boxSizing: "border-box",
+    position: "relative",
+  },
+});
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  globalStyles();
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
