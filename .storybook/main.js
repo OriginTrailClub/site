118b928c5fe5f768fbe1a1bf33273a6fd353f153
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2021-11-11 20:39:20 +0100

    Configure storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,13 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
-    "../components/**/*.stories.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../components/**/*.stories.mdx',
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules'];
+    return config;
+  },  
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,44 @@
+import { globalCss } from 'stitches.config';
+
+const globalStyles = globalCss({
+  html: {
+    height: '100%',
+    width: '100%',
+  },
+  body: {
+    margin: 0,
+    minHeight: '100%',
+    width: '100%',
+    fontFamily: '$default',
+    color: '$text-body',
+    '-webkit-font-smoothing': 'antialiased',
+    '-moz-osx-font-smoothing': 'grayscale',
+  },
+  '#__next': {
+    minHeight: '100%',
+    width: '100%',
+  },
+  '*': {
+    boxSizing: 'border-box',
+    position: 'relative',
+  },
+});
+
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (StoryComponent) => {
+    globalStyles();
+
+    return (<StoryComponent />);
+  },
+]
