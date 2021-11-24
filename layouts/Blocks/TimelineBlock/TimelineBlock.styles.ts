bd671d2c1f203a1855ccf4e00024e05617a89b5c
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2021-11-24 13:42:42 +0100

    Make it behave well responsively

--- layouts/Blocks/TimelineBlock/TimelineBlock.styles.ts
+++ layouts/Blocks/TimelineBlock/TimelineBlock.styles.ts
@@ -3,75 +3,99 @@ import { css } from 'stitches.config';
 import * as TimeLineBlockEventStyles from './TimelineBlockEvent.styles';
 
 export const container = css({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  position: 'relative',
-  width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    position: 'relative',
+    width: '100%',
 });
 
 export const contents = css({
-  marginTop: '$large',
-  display: 'flex',
-  width: '100%',
-  justifyContent: 'center',
+    '$$fadeSize': '$space$x-large',
+
+    marginTop: '$large',
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'center',
+
+    '&:after': {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        content: '',
+        background: 'linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) calc(0% + $$fadeSize), rgba(255,255,255,0) calc(100% - $$fadeSize), rgba(255,255,255,1) 100%)',
+        pointerEvents: 'none',
+    },
+
+    '@bp2': {
+        '$$fadeSize': '$space$xx-large',
+    },
+
+    '@bp4': {
+        maxWidth: ' max(90vw, 1600px)',
+    }
 });
 
 export const events = css({
-  $$eventCount: 0,
+    $$eventCount: 0,
 
-  $$gap: '$space$large',
+    $$gap: '$space$large',
 
-  $$eventSize: 'max(10vw, min(150px, 200px))',
-  
-  $$scrollPadding: 'calc((100vw - $$eventSize) / 2)',
-  $$scrollPaddingLeft: '$$scrollPadding',
-  $$scrollPaddingRight: '$$scrollPadding',
-  
-  display: 'grid',
-  gridTemplateColumns: '$$scrollPaddingLeft repeat($$eventCount, $$eventSize) $$scrollPaddingRight',
-  gridTemplateRows: '1fr',
-  gridAutoFlow: 'column',
-  margin: 0,
-  padding: 0,
+    // Size of a single event
+    $$eventSize: 'max(140px, 30vw)',
 
-  '&:before, &:after': {
-      content: '',
-  },
+    // Padding inside the scroll container
+    $$scrollPadding: 'calc((100vw - $$eventSize) / 2)',
+    $$scrollPaddingLeft: '$$scrollPadding',
+    $$scrollPaddingRight: '$$scrollPadding',
 
-  overflowX: 'auto',
-  overflowY: 'hidden',
-  scrollSnapType: 'x proximity',
+    display: 'grid',
+    gridTemplateColumns: '$$scrollPaddingLeft repeat($$eventCount, $$eventSize) $$scrollPaddingRight',
+    gridTemplateRows: '1fr',
+    gridAutoFlow: 'column',
+    margin: 0,
+    padding: 0,
 
-  // Hide scrollbar
-  MsOverflowStyle: 'none',
-  scrollbarWidth: 'none',
-  '&::-webkit-scrollbar': {
-    display: 'none',
-  },
+    '&:before, &:after': {
+        content: '',
+    },
 
-  '@bp2': {
-    $$scrollPadding: 'calc($space$regular - $space$small)',
-  },
+    overflowX: 'auto',
+    overflowY: 'hidden',
+    scrollSnapType: 'x mandatory',
 
-  [`.${TimeLineBlockEventStyles.container}`]: {
-      scrollSnapAlign: 'center',
-      px: '$small',
+    '@bp2': {
+        $$scrollPadding: 'calc(($$fadeSize) + $space$regular - $space$small)',
+        $$eventSize: 'max(180px, 15vw)',
+    },
 
-      '@bp2': {
-          scrollSnapAlign: 'start'
-      },
-  }
+    '@bp3': {
+        $$eventSize: 'min(220px, max(180px, 10vw))'
+    },
+
+    [`.${TimeLineBlockEventStyles.container}`]: {
+        scrollSnapAlign: 'center',
+        px: '$small',
+    },
+
+    // Hide scrollbar
+    MsOverflowStyle: 'none',
+    scrollbarWidth: 'none',
+    '&::-webkit-scrollbar': {
+        display: 'none',
+    },    
 });
 
 export const line = css({
-  position: 'absolute',
-  display: 'flex',
-  alignItems: 'center',
-  width: '100%',
-  height: 2,
-  bottom: 'calc((($fontSizes$cell + ($space$x-small * 2)) / 2) + 1px)',
-  background: `    
+    position: 'absolute',
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+    height: 2,
+    bottom: 'calc((($fontSizes$cell + ($space$x-small * 2)) / 2) + 1px)',
+    background: `    
     repeating-linear-gradient(90deg,
         transparent,
         transparent 3px,
--- layouts/Blocks/TimelineBlock/TimelineBlock.tsx
+++ layouts/Blocks/TimelineBlock/TimelineBlock.tsx
@@ -17,14 +17,16 @@ interface ITimelineBlockPropsComposition {
   Event: React.FC<TimelineBlockEventProps>;
 }
 
+// linear-gradient(to right,#fff 0,rgba(255,255,255,0.1) 10%,rgba(255,255,255,0.1) 90%,#fff 100%)
+
 export const TimelineBlock: React.FC<TimelineBlockProps> &
   ITimelineBlockPropsComposition = function TimelineBlock(props) {
     const { children } = props;
 
     return (
       <div className={Styles.container()}>
-        <div className={Styles.line()} />
         <div className={Styles.contents()}>
+        <div className={Styles.line()} />
           <ul className={Styles.events({
             css: {
               $$eventCount: React.Children.count(children),
