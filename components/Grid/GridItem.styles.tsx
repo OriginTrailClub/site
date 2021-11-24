d536b3dc7cbd0b1fc9e74c9d53cbec81135d8868
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2021-11-24 09:20:41 +0100

    Let overflow horizontal element not outgrow their parents

---
+++ components/Grid/GridItem.styles.tsx
@@ -0,0 +1,6 @@
+import { css } from 'stitches.config';
+
+export const container = css({
+  overflow: 'hidden',
+  width: '100%',
+});
--- components/Grid/GridItem.tsx
+++ components/Grid/GridItem.tsx
@@ -1,8 +1,14 @@
+import * as Styles from './GridItem.styles';
+
 export interface GridItemProps
   extends Omit<React.HTMLProps<HTMLDivElement>, 'className' | 'style'> {}
 
 export function GridItem(props: GridItemProps) {
   const { children } = props;
 
-  return <div {...props}>{children}</div>;
+  return (
+    <div className={Styles.container()} {...props}>
+      {children}
+    </div>
+  );
 }
