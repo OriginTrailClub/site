68e37812e8249625efa256e1628fc76c2dcdfa6d
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2021-11-26 18:09:20 +0100

    Add custom no-index header

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+      },
+    ]
+  },
 };
