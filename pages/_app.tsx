a8d9a8fbf8759684a942039d39f752184a56ecd1
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2021-11-27 14:40:31 +0100

    Add i18n provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
 import type { AppProps } from 'next/app';
 
-import { globalStyles } from 'constants/globalStyles';
-
 import { SSRProvider } from '@react-aria/ssr';
+import {I18nProvider} from '@react-aria/i18n';
+
+import { globalStyles } from 'constants/globalStyles';
 
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
 
   return (
-    <SSRProvider>
-      <Component {...pageProps} />
-    </SSRProvider>
+    <I18nProvider locale="en-US">
+      <SSRProvider>
+        <Component {...pageProps} />
+      </SSRProvider>
+    </I18nProvider>
   );
 }
 
