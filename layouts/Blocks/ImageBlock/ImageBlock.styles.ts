29539f9f7d092c0aa507763b6e161f17ae63863c
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2021-12-06 19:25:09 +0100

    Prettier Image Block

--- layouts/Blocks/ImageBlock/ImageBlock.styles.ts
+++ layouts/Blocks/ImageBlock/ImageBlock.styles.ts
@@ -1,16 +1,16 @@
 import { css } from 'stitches.config';
 
 export const container = css({
-    display: 'flex',
-    width: '100%',
-    px: '$regular',
-    justifyContent: 'center',
+  display: 'flex',
+  width: '100%',
+  px: '$regular',
+  justifyContent: 'center',
 });
 
 export const contents = css({
-    display: 'flex',
-    maxWidth: 1234,
-    width: '100%',
-    paddingBottom: 'min(calc(100% / 3 * 2), 360px)',
-    position: 'relative',
+  display: 'flex',
+  maxWidth: 1234,
+  width: '100%',
+  paddingBottom: 'min(calc(100% / 3 * 2), 360px)',
+  position: 'relative',
 });
--- layouts/Blocks/ImageBlock/ImageBlock.tsx
+++ layouts/Blocks/ImageBlock/ImageBlock.tsx
@@ -3,18 +3,18 @@ import Image from 'next/image';
 import * as Styles from './ImageBlock.styles';
 
 export interface ImageBlockProps {
-  src: string,
-  alt: string,
+  src: string;
+  alt: string;
 }
 
 export function ImageBlock(props: ImageBlockProps) {
-    const { src, alt } = props;
+  const { src, alt } = props;
 
-    return (
-        <div className={Styles.container()}>
-            <div className={Styles.contents()}>
-                <Image src={src} alt={alt} layout="fill" objectFit="contain" />
-            </div>
-        </div>
-    );
+  return (
+    <div className={Styles.container()}>
+      <div className={Styles.contents()}>
+        <Image src={src} alt={alt} layout="fill" objectFit="contain" />
+      </div>
+    </div>
+  );
 }
