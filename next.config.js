ee5dd0e5f3169a2e91804dfc38657dc523a98d00
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2022-02-16 19:48:57 +0100

    Add headers to every to dissalow indexing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,44 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+      },
+      {
+        source: 'learn/',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+      },
+      {
+        source: 'learn/courses/:course',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+      },
+      {
+        source: 'learn/courses/:course/:lesson',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+      },
+    ];
+  },
 };
