073402a433ab66ba00245404f796372d0c165a57
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2022-03-03 13:03:51 +0100

    Add embed element

---
+++ components/Embeds/YoutubeEmbed.styles.ts
@@ -0,0 +1,71 @@
+import { css } from 'stitches.config';
+
+export const aspectRatio = css({
+    paddingBottom: "56.25%",
+    height: 0,
+    width: "100%",
+});
+
+export const iframe = css({
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+})
+
+export const icon = css({
+    height: '$fontSizes$header-3',
+    width: '$fontSizes$header-3',
+    color: '$indigo-600',
+});
+
+
+export const trigger = css({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    backgroundColor: '$indigo-50',
+
+    p: '$regular',
+    borderRadius: '50%',
+
+    height: 'calc($space$regular * 2 + $fontSizes$header-3)',
+    width: 'calc($space$regular * 2 + $fontSizes$header-3)',
+});
+
+export const placeholder = css({
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+
+    p: 0,
+    border: 'transparent',
+
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    cursor: 'pointer',
+
+    "&:hover, &:focus": {
+        [`& .${icon()}`]: {
+            color: '$indigo-700',
+        },
+    },
+});
+
+export const label = css({
+    fontFamily: '$mono',
+    fontSize: '$caption',
+    lineHeight: '$none',
+    color: '$text-subtle',
+
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'center',
+    py: '$regular'
+});
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['img.youtube.com'],
+  },
   async headers() {
     return [
       {
