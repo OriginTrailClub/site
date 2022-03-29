8b5ff0a143a4aec9cea00c778ef65eebcf4f6a72
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2022-03-29 11:12:27 +0200

    Add link to content

--- components/MDXContent/MDXContent.tsx
+++ components/MDXContent/MDXContent.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+
 import {
   Heading,
   HeadingProps,
@@ -11,8 +13,11 @@ import {
   Italic,
   ItalicProps,
   Block,
+  Link,
+  LinkProps,
 } from 'components/Typography';
 import { YoutubeEmbed, YoutubeEmbedProps } from 'components/Embeds';
+import { useRouter } from 'next/router'
 
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
 import { slugify } from 'utils/slugify';
@@ -25,6 +30,27 @@ function Heading2(props: Omit<HeadingProps, 'level'>) {
   }
 
   return <Heading id={id} {...props} level={2} />;
+}
+
+function MDLink(props: LinkProps) {
+  const { href } = props;
+
+  const router = useRouter();
+
+  console.log(router)
+
+  const isInternal = React.useMemo(() => {
+    return href[0] === '/';
+  }, [href]);
+
+  if (isInternal) {
+    return (<Link {...props} />)
+  }
+
+  return (<Link {...props} target="_blank" rel="noopener noreferrer" />)
+
+
+  // Internal / External
 }
 
 const components = {
@@ -44,6 +70,8 @@ const components = {
 
   strong: (props: BoldProps) => <Bold {...props} />,
   em: (props: ItalicProps) => <Italic {...props} />,
+
+  a: (props: LinkProps) => <MDLink {...props} />,
 
   YoutubeEmbed: (props: YoutubeEmbedProps) => (
     <Block>
