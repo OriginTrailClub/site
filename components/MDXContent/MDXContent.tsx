009edc79483d82d9651e904420cb42524576eef5
Jaco Bovenschen <jaco1994bovenschen@gmail.com>
2022-03-29 11:16:20 +0200

    Wrap Links in MDX in next/link

--- components/MDXContent/MDXContent.tsx
+++ components/MDXContent/MDXContent.tsx
@@ -17,7 +17,7 @@ import {
   LinkProps,
 } from 'components/Typography';
 import { YoutubeEmbed, YoutubeEmbedProps } from 'components/Embeds';
-import { useRouter } from 'next/router'
+import NextLink from 'next/link'
 
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
 import { slugify } from 'utils/slugify';
@@ -35,22 +35,21 @@ function Heading2(props: Omit<HeadingProps, 'level'>) {
 function MDLink(props: LinkProps) {
   const { href } = props;
 
-  const router = useRouter();
-
-  console.log(router)
-
   const isInternal = React.useMemo(() => {
     return href[0] === '/';
   }, [href]);
 
+  let el;
+
   if (isInternal) {
-    return (<Link {...props} />)
+    el = (<Link {...props} />)
+  } else {
+    el = (<Link {...props} target="_blank" rel="noopener noreferrer" />);
   }
 
-  return (<Link {...props} target="_blank" rel="noopener noreferrer" />)
-
-
-  // Internal / External
+  return (
+    <NextLink href={props.href} passHref>{el}</NextLink>
+  )
 }
 
 const components = {
